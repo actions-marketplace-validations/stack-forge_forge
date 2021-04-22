a4a0adc2a6a9dea65cd8bc153c8cc02759f97f53
Yan Mendes <ymendesf@gmail.com>
2021-04-22 18:00:28 -0300

    fix: url

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function run () {
     const outFile = 'output.tgz'
     const writer = fs.createWriteStream(outFile)
     await axios
-      .post('http://localhost:3050/v1/transform', form, {
+      .post('https://api.stackforge.tech/v1/transform', form, {
         responseType: 'stream',
         headers: {
           Authorization: `Bearer ${apiKey}`,
